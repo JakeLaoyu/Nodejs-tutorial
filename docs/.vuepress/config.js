73dc6f208b89605b49d7bbc4755bb371a79a2f19
Jake <yucj@dxy.cn>
2018-05-15 15:11:32 +0800

    feat: set ico

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,9 @@ module.exports = {
         editLinks: true,
         editLinkText: '帮助改善此页面！',
         docsDir: 'docs',
+        head: [
+            ['link', { rel: 'icon', href: '/favicon.ico' }]
+        ],
         nav: [{
                 text: '主页',
                 link: '/'
